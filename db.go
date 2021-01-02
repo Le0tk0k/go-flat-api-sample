2d681194f6230f5efe749926e8dcfd92e93c0453
Leo Takaoka <le0tk0k@gmail.com>
2021-01-02 15:27:52 +0900

    update: newDb()でerrorを返す

--- db.go
+++ db.go
@@ -1,11 +1,14 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
-func newDb() *sql.DB {
+func newDb() (*sql.DB, error) {
 	db, err := sql.Open("mysql", "user:password@tcp(db:3306)/sample?charset=utf8")
 	if err != nil {
-		panic(err.Error())
+		return nil, fmt.Errorf("failed to Open mysql: %w", err)
 	}
-	return db
+	return db, nil
 }
