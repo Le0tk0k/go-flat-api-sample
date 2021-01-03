f7b030df77596cfdbe6ddd246c5ef3d09630f868
Leo Takaoka <le0tk0k@gmail.com>
2021-01-03 11:58:11 +0900

    update: newDb()で通信確認

--- db.go
+++ db.go
@@ -8,7 +8,12 @@ import (
 func newDb() (*sql.DB, error) {
 	db, err := sql.Open("mysql", "user:password@tcp(db:3306)/sample?charset=utf8")
 	if err != nil {
-		return nil, fmt.Errorf("failed to Open mysql: %w", err)
+		return nil, fmt.Errorf("failed to open mysql: %w", err)
 	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping: %w", err)
+	}
+
 	return db, nil
 }
