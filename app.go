ac949412f5f9c284340259ae5aa8ff8642171599
Leo Takaoka <le0tk0k@gmail.com>
2021-01-06 21:40:05 +0900

    update: db.goをapp.goに統合

--- app.go
+++ app.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 )
@@ -20,4 +23,17 @@ func (a *app) initRoutes() {
 
 func (a *app) run() {
 
+}
+
+func newDb() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "user:password@tcp(db:3306)/sample?charset=utf8")
+	if err != nil {
+		return nil, fmt.Errorf("failed to open mysql: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping: %w", err)
+	}
+
+	return db, nil
 }
