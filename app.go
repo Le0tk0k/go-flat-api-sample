552b6d285eb55baa76e051ccab12f07aaad094d4
Leo Takaoka <le0tk0k@gmail.com>
2021-01-08 16:08:52 +0900

    update: sqlxやめた

--- app.go
+++ app.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/jmoiron/sqlx"
 )
 
 type app struct {
 	router *mux.Router
-	db     *sqlx.DB
+	db     *sql.DB
 }
 
 func (a *app) init() {
@@ -34,8 +34,8 @@ func (a *app) run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, a.router))
 }
 
-func newDb() (*sqlx.DB, error) {
-	db, err := sqlx.Open("mysql", "user:password@tcp(db:3306)/sample?charset=utf8")
+func newDb() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "user:password@tcp(db:3306)/sample?charset=utf8")
 	if err != nil {
 		return nil, fmt.Errorf("failed to open mysql: %w", err)
 	}
